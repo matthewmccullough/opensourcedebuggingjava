2734716332d32b94b18b837c5e5f3e3c84a6e703
Matthew McCullough <matthewm@ambientideas.com>
2009-09-04 19:16:27 -0600

    Adding jtracert sample for Java2d

--- jtracert-samples/jtracert-java2d.sh
+++ jtracert-samples/jtracert-java2d.sh
@@ -5,7 +5,7 @@ export JTRACERT_HOME=/Applications/Dev/jtracert
 #Run java2d with jtracert listener
 #java -jar $JAVA2D_HOME/Java2D.jar -javaagent:$JTRACERT_HOME/jTracert.jar=7007 &
 #java -javaagent:$JTRACERT_HOME/jTracert.jar -DclassNameRegEx=.* -DverboseInstrumentation -DverboseAnalyze -DanalyzerOutput=webSequenceDiagramsOut -DoutputFolder=/Users/mccm06/Documents/Teach/Courses/Open-Source-Debugging-CombinedTools-NFJS/example-files_git/jtracert-samples com.ambientideas.HelloWorldJava
-java -javaagent:$JTRACERT_HOME/jTracert.jar=7007 -DclassNameRegEx=.* -DverboseInstrumentation -DverboseAnalyze com.ambientideas.HelloWorldJava &
+java -jar /Developer/Examples/Java/JFC/Java2D/Java2D.jar -javaagent:$JTRACERT_HOME/jTracert.jar=7007 -DclassNameRegEx=.* -DverboseInstrumentation -DverboseAnalyze
 APPPID=$!
 
 cd $JTRACERT_HOME
